ad7afa7095e3944dfb9bc14117835ed40b4959c3
Linngdu664 <664774652@qq.com>
2025-03-19 01:47:59 +0800

    1.1.2 键位调整

--- src/main/java/com/linngdu664/drglaserpointer/registry/KeyMappingRegister.java
+++ src/main/java/com/linngdu664/drglaserpointer/registry/KeyMappingRegister.java
@@ -10,7 +10,7 @@ import org.lwjgl.glfw.GLFW;
 
 @EventBusSubscriber(modid = Main.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class KeyMappingRegister {
-    public static final KeyMapping SWITCH_TO_LASER_POINTER = new KeyMapping("key.laserpointer.switch_to_laser_pointer", GLFW.GLFW_KEY_LEFT_CONTROL, "key.categories.misc");
+    public static final KeyMapping SWITCH_TO_LASER_POINTER = new KeyMapping("key.laserpointer.switch_to_laser_pointer", GLFW.GLFW_KEY_Z, "key.categories.laserpointer");
 
     @SubscribeEvent
     public static void registerKeyBindings(RegisterKeyMappingsEvent event) {
